5733d9805e3ff5ee89c27deb26b2bf5ea46d4245
michaelpupulin <michael.l.pupulin@gmail.com>
2022-10-19 10:03:15 -0400

    second

--- build.sbt
+++ build.sbt
@@ -1,8 +1,18 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.10"
+
+ThisBuild / scalaVersion := "2.11.0"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "goodreads"
+    name := "goodreads",
+    libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.4.8",
+    libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.4.8",
+    libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "3.3.2",
+    libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "3.3.2",
+    libraryDependencies += "org.apache.hadoop" % "hadoop-aws" % "3.3.2",
+    //libraryDependencies += "org.apache.hadoop" % "hadoop-cloud" % "2.7.3",
+    libraryDependencies += "com.amazonaws" % "aws-java-sdk-pom" % "1.12.315",
+    //libraryDependencies +="com.github.jkugiya" %% "aws-v4-signer-scala" % "0.13"
+      libraryDependencies += "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.13.0"
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,87 @@
+import org.apache.log4j.Logger._
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, SparkSession}
 object Main {
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+  val spark: SparkSession = SparkSession
+    .builder
+    .appName("goodreads")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+
+  // Replace Key with your AWS account key (You can find this on IAM
+  spark.sparkContext
+    .hadoopConfiguration.set("fs.s3a.access.key", "Access")
+  // Replace Key with your AWS secret key (You can find this on IAM
+  spark.sparkContext
+    .hadoopConfiguration.set("fs.s3a.secret.key", "Secret")
+  spark.sparkContext
+    .hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
+
+
+
+//for aws read
+//  val df: DataFrame = spark.read.option("inferSchema","true")
+//    .json("s3a://pupulin-goodreads/goodreads_books.json")
+  //set access key
+//for local read
+    val df: DataFrame = spark.read.option("inferSchema","true")
+      .json("goodreads_books.json")
+
+
+  df.show(5)
+//
+//  val dfavg: DataFrame = df.select(col("average_rating").cast("double"),col("publisher"),col("publication_year"))
+//    .groupBy(col("publisher"))
+//    .mean("average_rating")
+
+
+ // dfavg.show(4)
+
+
+
+
+//  val dfPR = df.filter(col("publisher")==="Penguin Books" ||
+//    col("publisher")==="Random House" ||
+//    col("publisher")==="Penguin Random House")
+//    .select(col("publication_year"),col("publisher"))
+//    .groupBy("publication_year")
+//    .count()
+//
+//
+//
+//  val dfSS = df.filter(col("publisher")==="Simon & Schuster")
+//    .select(col("publication_year"),col("publisher"))
+//    .groupBy("publication_year")
+//    .count()
+
+//  dfSS.show(10)
+
+  val SSavg_year: DataFrame = df.filter(col("publisher")==="Penguin Books" ||
+    col("publisher")==="Random House" ||
+    col("publisher")==="Penguin Random House")
+    .select(col("publication_year"), col("average_rating").cast("double"))
+    .groupBy(col("publication_year"))
+    .mean("average_rating")
+
+  SSavg_year.show(10)
+
+  val PRavg_year: DataFrame = df.filter(col("publisher")==="Simon & Schuster")
+    .select(col("publication_year"), col("average_rating").cast("double"))
+    .groupBy(col("publication_year"))
+    .mean("average_rating")
+
+  PRavg_year.show(10)
+//  //writing
+//  dfavg.coalesce(1).write.csv("s3a://pupulin-goodreads/avg_all.csv")
+  PRavg_year.coalesce(1).write.csv("s3a://pupulin-goodreads/avg_pr_by_yr.csv")
+  SSavg_year.coalesce(1).write.csv("s3a://pupulin-goodreads/avg_ss_by_yr.csv")
+//  dfPR.coalesce(1).write.csv("s3a://pupulin-goodreads/pr_yr.csv")
+//  dfSS.coalesce(1).write.csv("s3a://pupulin-goodreads/ss_yr.csv")
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("done")
   }
 }
